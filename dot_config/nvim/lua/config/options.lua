ad01980903784c32d22addeb6b1306a367e9032f
Var Bhat <mailvarbhat@gmail.com>
2024-04-28 16:53:42 +0530

    fix(sway): refactor sway config

--- dot_config/nvim/lua/config/options.lua
+++ dot_config/nvim/lua/config/options.lua
@@ -3,5 +3,8 @@ vim.opt.foldenable = false
 vim.opt.foldmethod = "expr" -- "indent"
 vim.opt.foldexpr = "v:lua.vim.treesitter.foldexpr()"
 
+-- Enable Title
+vim.opt.title = true
+
 -- LSP Settings
 vim.g.lazyvim_python_lsp = "basedpyright"
