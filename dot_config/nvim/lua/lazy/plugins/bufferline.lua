609beee20ac11a81f88a774e7254dbfc1311175b
Var Bhat <mailvarbhat@gmail.com>
2024-08-03 01:44:13 +0530

    ft: neovim

--- dot_config/nvim/lua/lazy/plugins/bufferline.lua
+++ dot_config/nvim/lua/lazy/plugins/bufferline.lua
@@ -3,6 +3,7 @@ return {
     'akinsho/bufferline.nvim',
     version = '*',
     dependencies = 'nvim-tree/nvim-web-devicons',
+    event = 'BufEnter',
     config = function()
       require('bufferline').setup {
         options = {
