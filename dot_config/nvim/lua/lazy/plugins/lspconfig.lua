8f8b6ca14fb68fb9ae8220d352615f6f3772209a
Var Bhat <mailvarbhat@gmail.com>
2024-08-03 01:48:44 +0530

    ft: neovim

--- dot_config/nvim/lua/lazy/plugins/lspconfig.lua
+++ dot_config/nvim/lua/lazy/plugins/lspconfig.lua
@@ -104,6 +104,10 @@ return {
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
+          -- Code Lens
+          map('<leader>cn', vim.lsp.codelens.run, '[C]ode Le[n]s')
+          map('<leader>cr', vim.lsp.codelens.refresh, '[C]ode Lens [r]efresh')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
