69f1e07b6261d8e5c07ea9c7b6b31ddf411991f3
varbhat <mailvarbhat@gmail.com>
2025-12-29 22:39:57 +0530

    add dconf settings

--- home/home.nix
+++ home/home.nix
@@ -114,7 +114,7 @@
     enable = true;
     settings = {
       "org/gnome/desktop/interface".color-scheme = "prefer-dark";
-      "org/gnome/desktop/wm/preferences".button-layout = "minimize,maximize,close";
+      "org/gnome/desktop/wm/preferences".button-layout = ":minimize,maximize,close";
     };
   };
 
